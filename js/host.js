9e4e379b9713c48d004c41a4eb8dc1d5065211e6
Your Name <you@example.com>
2024-10-01 03:46:31 +0300

    update

--- js/host.js
+++ js/host.js
@@ -1,17 +1,20 @@
-import TelegramBot from 'node-telegram-bot-api';
-import express from 'express';
+const TelegramBot = require('node-telegram-bot-api');
 
-const app = express();
+// Замените 'YOUR_TELEGRAM_BOT_TOKEN' на токен вашего бота
 const token = '7700449957:AAHgFLxoRS7aDebn1cqM_RclUT_DJlDo23M';
 const bot = new TelegramBot(token, { polling: true });
 
-// Обработка команды /start
+// Команда для начала игры
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Привіт! Грати можна за цим посиланням: [Золотая Лихорадка](https://ваш_сайт.com)', { parse_mode: 'Markdown' });
+    const options = {
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: "Начать игру", url: "https://github.com/artem23lil/GoldRush" }]
+            ]
+        }
+    };
+    bot.sendMessage(chatId, "Добро пожаловать в Золотую Лихорадку! Нажмите кнопку ниже, чтобы начать игру.", options);
 });
 
-// Запуск сервера
-app.listen(3000, () => {
-    console.log('Сервер запущен на http://localhost:3000');
-});
+// Остальной код...
